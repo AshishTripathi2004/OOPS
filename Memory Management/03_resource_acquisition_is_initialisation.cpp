9d634d0fc5b18fc502c39bfd0a184ffe8549ec06
AshishTripathi2004 <161132058+AshishTripathi2004@users.noreply.github.com>
2026-04-08 22:50:33 +0530

    Updated

--- Memory Management/03_resource_acquisition_is_initialisation.cpp
+++ Memory Management/03_resource_acquisition_is_initialisation.cpp
@@ -22,6 +22,13 @@ int main(){
     // MyClass obj() does not create an object
     // this is the most vexing parse quirk in C++
     // RESOLUTION: avoid parentheses at all or use curly braces
+
+    /*
+        The "most vexing parse" in C++ is a specific form of 
+        syntactic ambiguity where a piece of code can be interpreted 
+        as either a function declaration or an object instantiation.
+    */
+   
     MyClass obj{};
     MyClass obj1;
     return 0;
---
+++ Memory Management/06_dynamic_memory_allocation.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+   1. malloc(Memory Allocation)
+   -- Purpose: Allocates size bytes of uninitialized memory.
+   -- Returns: A pointer to the allocated memory or NULL if allocation fails.
+
+   2. calloc(Contiguous Allocation)
+   -- Purpose: Allocates memory for num elements of size bytes each, initializes all to 0.
+   -- Returns: Pointer or NULL.
+
+   3. realloc(Re-allocation)
+   -- Purpose: Changes the size of previously allocated memory.
+   -- Returns: Pointer to the new memory (may be same or different), NULL if fails.
+   -- relloc may move the memory block; old pointer may become invalid.
+   -- If realloc fails, it returns NULL but original memory is not freed, so you must handle it carefully.
+*/
+
+int main(){
+    return 0;
+}
